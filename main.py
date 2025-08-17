d0e13b426a1a7e7b1a6a85808a56c2bc26a7037f
Lachlan Rixon <lachlan_rixon@icloud.com>
2025-08-18 01:09:07 +1000

    Completed Ch1

--- main.py
+++ main.py
@@ -1,20 +1,48 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
-
-
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-
-client = genai.Client(api_key=api_key)
+from google.genai import types
 
 def main():
+    load_dotenv()
+    
+    verbose = "--verbose" in sys.argv
+    args = []
+    for arg in sys.argv[1:]:
+        if not arg.startswith("--"):
+            args.append(arg)
+
+    if not args: 
+        print("AI Code Assistant")
+        print('\nUsage: uv run main.py "Your input here"')
+        sys.exit(1)
+
+    user_prompt = " ".join(args)
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+    
+    if verbose:
+        print(f"User prompt: {user_prompt}")
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
+    generate_content(client, messages, verbose)
+
+def generate_content(client, messages, verbose):
     response = client.models.generate_content(
-        model = "gemini-2.0-flash-001", contents = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+        model = "gemini-2.0-flash-001", 
+        contents = messages,
     )
+    if verbose:
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
+    print("Response:")
     print(response.text)
-    print("Prompt tokens: " + f"{response.usage_metadata.prompt_token_count}")
-    print("Response tokens: " + f"{response.usage_metadata.candidates_token_count}")
+
+
 
 if __name__ == "__main__":
     main()
